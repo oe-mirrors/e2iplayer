e7e8e207c28b82419f51cca3c51bce9991c68430
Max <maxbambi@libero.it>
2019-07-29 21:27:53 +0200

    Fix path to get resources: http://e2iplayer.pkteam.pl/

--- IPTVPlayer/tools/iptvtools.py
+++ IPTVPlayer/tools/iptvtools.py
@@ -53,7 +53,7 @@ def GetUpdateServerUri(file='', serverNum=None):
 
 def GetResourcesServerUri(file='', serverNum=None):
     serverKey = GetServerKey(serverNum)
-    uri = SERVER_DOMAINS[serverKey] + 'resources/' + file
+    uri = 'http://e2iplayer.pkteam.pl/resources/' + file
     printDBG("GetResourcesServerUri -> %s" % uri)
     return uri
 
