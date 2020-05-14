ba8ca8633471ffffbe4362406647e660c54cc5fe
Max <maxbambi@libero.it>
2020-05-14 12:59:30 +0200

    fix altadefinizione.us

--- IPTVPlayer/hosts/hostaltadefinizionecool.py
+++ IPTVPlayer/hosts/hostaltadefinizionecool.py
@@ -19,7 +19,7 @@ except Exception: import simplejson as json
 ###################################################
 
 def gettytul():
-    return 'https://altadefinizione.town/'
+    return 'https://altadefinizione.us/'
 
 class AltadefinizioneCool(CBaseHostClass):
  
@@ -29,9 +29,9 @@ class AltadefinizioneCool(CBaseHostClass):
         self.HEADER = {'User-Agent': self.USER_AGENT, 'DNT':'1', 'Accept': 'text/html'}
         self.AJAX_HEADER = dict(self.HEADER)
         self.AJAX_HEADER.update( {'X-Requested-With': 'XMLHttpRequest'} )
-        self.MAIN_URL = 'https://altadefinizione.town/'
+        self.MAIN_URL = 'https://altadefinizione.us/'
         self.AZ_URL = self.MAIN_URL + 'catalog/%l/page/{0}'
-        self.DEFAULT_ICON_URL = 'https://altadefinizione.town/templates/Dark/img/logo.png'
+        self.DEFAULT_ICON_URL = 'https://altadefinizione.us/templates/Dark/img/logo.png'
         
         self.cacheCategories = []
         
