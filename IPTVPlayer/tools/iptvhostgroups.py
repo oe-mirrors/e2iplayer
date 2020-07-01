e874cf2be37e240c27c49f424ea528b1be3fadbf
Max <maxbambi@libero.it>
2020-07-01 21:23:34 +0200

    add cda-filmy

--- IPTVPlayer/tools/iptvhostgroups.py
+++ IPTVPlayer/tools/iptvhostgroups.py
@@ -87,6 +87,7 @@ class IPTVHostsGroups:
         'cb01' : [ 'italian', 'moviesandseries'] ,
         'cb01uno' : [ 'italian', 'moviesandseries'] ,
         'cdapl' : [ 'moviesandseries', 'others', 'polish'] ,
+        'cdafilmy' : [ 'moviesandseries', 'polish'] ,
         'chomikuj' : [ 'others', 'polish'] ,
         'christusvincit' : [ 'polish'] ,
         'cimaclubcom' : [ 'arabic', 'moviesandseries'] ,
