2a7e069a081763f8f883b81b829248cb1fe2d226
jbleyel <jbleyel@gmx.net>
2022-02-15 20:47:27 +0100

    add hostinfoversion

--- IPTVPlayer/hosts/hostzaluknijcc.py
+++ IPTVPlayer/hosts/hostzaluknijcc.py
@@ -68,7 +68,7 @@ class Zaluknij(CBaseHostClass):
 #                        {'category': 'list_items', 'title': _('Children'), 'url': self.getFullUrl('/genre/anime-bajki/')},
                         {'category': 'list_items', 'title': _('Series'), 'url': self.getFullUrl('/series/index/')},
                         {'category': 'list_years', 'title': _('Filter By Year'), 'url': self.getFullUrl('/filmy-online/')},
-                        {'category': 'list_cats',  'title': _('Movies genres'), 'url': self.getFullUrl('/filmy-online/')},
+                        {'category': 'list_cats', 'title': _('Movies genres'), 'url': self.getFullUrl('/filmy-online/')},
 #                        {'category':'list_az',        'title': _('Alphabetically'),    'url':self.MAIN_URL},
                         {'category': 'search', 'title': _('Search'), 'search_item': True},
                         {'category': 'search_history', 'title': _('Search history')}, ]
@@ -149,7 +149,6 @@ class Zaluknij(CBaseHostClass):
             nextPage = True
         else:
             nextPage = False
-
 
         if 'wyszukiwarka?phrase=' in cItem['url']:
             data = self.cm.ph.getDataBeetwenNodes(data, ('<div', '>', 'advanced-search'), ('<footer', '>'))[1]
