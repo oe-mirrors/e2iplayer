22bf3fae1af71e62d24ba9f3c9f6f1988bf5c4e9
jbleyel <jbleyel@gmx.net>
2025-06-29 10:17:43 +0200

    text updates

--- IPTVPlayer/components/iptvplayerwidget.py
+++ IPTVPlayer/components/iptvplayerwidget.py
@@ -457,7 +457,7 @@ class E2iPlayerWidget(Screen):
                         message = _('It seems that the host "%s" has crashed. Do you want to report this problem?') % self.hostName
                         message += "\n"
                         message += _('\nMake sure you are using the latest version of the plugin.')
-                        message += _('\nYou can also report problem here: \n https://gitlab.com/zadmario/e2iplayer/issues')
+                        message += _('\nYou can also report problem here: \n https://github.com/oe-mirrors/e2iplayer/issues')
                         self.session.openWithCallback(self.reportHostCrash, MessageBox, text=message, type=MessageBox.TYPE_YESNO)
             self.hideSpinner()
         except Exception:
