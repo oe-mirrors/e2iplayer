e7da18078eddd614911d61e0289fcf0f00c92f84
Masta2002 <81389620+Masta2002@users.noreply.github.com>
2025-06-29 18:12:22 +0200

    change from gitlab to github

--- IPTVPlayer/components/iptvsubdownloader.py
+++ IPTVPlayer/components/iptvsubdownloader.py
@@ -385,7 +385,7 @@ class IPTVSubDownloaderWidget(Screen):
                     message = _("It seems that the subtitle's provider \"%s\" has crashed. Do you want to report this problem?") % self.hostName
                     message += "\n"
                     message += _('\nMake sure you are using the latest version of the plugin.')
-                    message += _('\nYou can also report problem here: \nhttps://gitlab.com/iptvplayer-for-e2/iptvplayer-for-e2/issues\nor here: samsamsam@o2.pl')
+                    message += _('\nYou can also report problem here: \nhttps://github.com/oe-mirrors/e2iplayer/issues')
                     self.session.openWithCallback(self.reportHostCrash, MessageBox, text=message, type=MessageBox.TYPE_YESNO)
             self.hideSpinner()
         except Exception:
