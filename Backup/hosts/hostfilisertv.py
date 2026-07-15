09750b972463c683a25d9f234dd58cd49eb53ca8
jbleyel <jbleyel@gmx.net>
2026-07-15 16:07:08 +0200

    fix typo

--- Backup/hosts/hostfilisertv.py
+++ Backup/hosts/hostfilisertv.py
@@ -113,7 +113,7 @@ class FiliserTv(CBaseHostClass):
         addFilter(tmpData, 'genres', True, '', 'data-gen')
 
         # year
-        self.cacheFilters['year'] = [{'title': _('Year:') + " " _('Any')}]
+        self.cacheFilters['year'] = [{'title': _('Year:') + " " + _('Any')}]
         year = datetime.now().year
         while year >= 1978:
             self.cacheFilters['year'].append({'title': _('Year:') + " " + str(year), 'year': year})
