fec966a6c8765c18318e1e51a436fabf63f0e226
jbleyel <jbleyel@gmx.net>
2026-08-15 08:27:05 +0200

    fix tab/space mix

--- IPTVPlayer/__init__.py
+++ IPTVPlayer/__init__.py
@@ -5,18 +5,18 @@ from Tools.Directories import resolveFilename, SCOPE_PLUGINS
 from gettext import bindtextdomain, dgettext, gettext
 
 try:
-	from enigma import eListbox
-	from Components.SystemInfo import BoxInfo
-	GRIDSUPPORT = eListbox.orGrid is not None and BoxInfo.getItem("distro") in ("openatv",)
+    from enigma import eListbox
+    from Components.SystemInfo import BoxInfo
+    GRIDSUPPORT = eListbox.orGrid is not None and BoxInfo.getItem("distro") in ("openatv",)
 except AttributeError:
-	GRIDSUPPORT = False
+    GRIDSUPPORT = False
 
 PluginLanguageDomain = "IPTVPlayer"
 PluginLanguagePath = "Extensions/IPTVPlayer/locale"
 
 
 def localeInit():
-	bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
+    bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
 
 
 def _(txt):
